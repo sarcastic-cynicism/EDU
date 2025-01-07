da5774e0088c8af6a0052000e17c9a029aba0d53
cts <columsandford@gmail.com>
2025-01-07 12:23:58 -0600

    first commit

---
+++ langAndOllama.py
@@ -0,0 +1,13 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
+template = """Question: {question}
+              Answer: Let's think step by step"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = OllamaLLM(model="llama3.2")
+
+chain = prompt | model 
+
+print(chain.invoke({"question": "what is langchain"}))
